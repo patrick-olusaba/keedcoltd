1bbfc8d32a8579e19180d6f928e760ea9baf8fd9
Patrick Olusaba <patrick.olusaba@gmail.com>
2026-04-24 23:02:29 +0300

    new changes22

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -29,7 +29,7 @@ const Footer: React.FC = () => {
       </div>
       <div className="footer__bar">
         <div className="container">
-          <p>© {new Date().getFullYear()} keedcoltd.com.</p>
+          <p>© {new Date().getFullYear()} patrick.olusaba@gmail.com</p>
         </div>
       </div>
     </footer>
